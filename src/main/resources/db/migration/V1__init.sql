f0bf739e02494fb309344c117a6636f468d79105
Hugo.Leon <hug.leon@duocuc.cl>
2026-06-03 22:34:05 -0400

    datos para insert en bd inicial

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,6 +7,15 @@ CREATE TABLE productos (
                            estado VARCHAR(20) DEFAULT 'Disponible'
 );
 
-INSERT INTO productos (sku, nombre, precio, stock, estado) VALUES
-                                                               ('CUAD-UNI-01', 'Cuaderno Universitario', 1500, 50, 'Disponible'),
-                                                               ('LAP-PAST-02', 'Lápiz Pasta Azul', 300, 100, 'Disponible');
+
+INSERT INTO productos (nombre, precio, stock) VALUES
+                                                  ('Teclado Gamer Redragon', 24990, 15),
+                                                  ('Monitor Samsung 24 pulgadas', 129990, 8),
+                                                  ('Mouse Logitech G203', 19990, 20),
+                                                  ('Audífonos HyperX Cloud', 45000, 12),
+                                                  ('Silla Gamer Cougar', 150000, 5),
+                                                  ('Disco SSD 1TB WD', 60000, 30),
+                                                  ('Memoria RAM 16GB DDR4', 40000, 25),
+                                                  ('Tarjeta de Video RTX 3060', 350000, 4),
+                                                  ('Placa Madre ASUS B550', 110000, 10),
+                                                  ('Fuente de Poder 600W EVGA', 55000, 18);
